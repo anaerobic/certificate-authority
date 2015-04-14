abf986720bf979db4aeb82afbb0a2cf9ffc41cf6
mwmartin <mwm.cal@gmail.com>
2015-04-14 12:06:22 -0600

    switching to lacolhost.com and including a simple owin app

---
+++ OwinApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace OwinApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "https://+:443";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Listening at: " + url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
